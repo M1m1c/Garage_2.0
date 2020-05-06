89760e60da3f8faad3e6be37d655f611953e4c72
Jörgen Forsberg <j.forsberg65@outlook.com>
2020-05-06 13:29:18 +0200

    Added vehicle data annotations

--- Garage_2.0/Models/Vehicle.cs
+++ Garage_2.0/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,20 +8,35 @@ namespace Garage_2._0.Models
 {
     public class Vehicle
     {
-        public int Id { get; set; }
 
+        
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Typ")]
         public EnumType VehicleType { get; set; }
 
+        [Required]
+        [Display(Name = "Regnummer")]
         public string RegNum { get; set; }
 
+        [Required]
+        [Display(Name ="Färg")]
         public EnumColor Color { get; set; }
-
+        
+        [Required]
+        [Display(Name = "Fabrikat")]
         public string Brand { get; set; }
 
+        [Required]
+        [Display(Name = "Modell")]
         public string Model { get; set; }
 
+        [Required]
+        [Display(Name = "Hjul")]
+        [Range(0,10)]
         public int Wheels { get; set; }
 
+        [Display(Name = "Ankomst")]
         public DateTime ArrivalTime { get; set; }
     }
 
