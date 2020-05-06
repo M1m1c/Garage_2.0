8d48e3c859c8552404fc6202f8796f354a20fcb3
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-05-06 11:26:32 +0200

    First database migration started on SeedData

---
+++ Garage_2.0/Migrations/20200506090131_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage_2._0.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleType = table.Column<int>(nullable: false),
+                    RegNum = table.Column<string>(nullable: true),
+                    Color = table.Column<int>(nullable: false),
+                    Brand = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Wheels = table.Column<int>(nullable: false),
+                    ArrivalTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
---
+++ Garage_2.0/Models/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2._0.Models
+{
+    public static class SeedData
+    {
+        public static void Initalise(IServiceProvider serviceProvider)
+        {
+           /* using (var Context= new Garage_2_0Context)
+
+               
+                    if (_context.Vehicle.Any())
+                    {
+                        return;
+                    }
+
+                    _context.Vehicle.AddRange(
+                        new Vehicle
+                        {
+                            VehicleType = EnumType.Bil,
+                            RegNum = "ABC123",
+                            Color = EnumColor.Blå,
+                            Brand = "BMW",
+                            Model = "1",
+                            Wheels = 4,
+                            ArrivalTime = DateTime.Now
+                        },
+                         new Vehicle
+                         {
+                             VehicleType = EnumType.Bil,
+                             RegNum = "DBC123",
+                             Color = EnumColor.Röd,
+                             Brand = "Audi",
+                             Model = "1",
+                             Wheels = 4,
+                             ArrivalTime = DateTime.Now
+                         }
+                        );
+                    _context.SaveChanges();*/
+                
+        }
+    }
+}
