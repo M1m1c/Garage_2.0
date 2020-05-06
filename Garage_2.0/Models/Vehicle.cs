38e72189c73559bfddcfe3a92d662d621580c4e7
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-05-06 10:33:11 +0200

    Added Vehicle class with enums

---
+++ Garage_2.0/Models/Vehicle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public EnumType VehicleType { get; set; }
+
+        public string RegNum { get; set; }
+
+        public EnumColor Color { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Wheels { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+    }
+
+    public enum EnumType
+    {
+        Bil,
+        MC,
+        Buss,
+        Båt,
+        Flygplan,
+        Cykel
+    }
+    
+    public enum EnumColor
+    {
+        Svart,
+        Blå,
+        Röd,
+        Grön,
+        Gul,
+        Grå,
+        Orange,
+        Vit,
+        Annan
+    }
+}
