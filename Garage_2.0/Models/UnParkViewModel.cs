47de5c4af4d687c208580cf74b3f22a384319593
Jörgen Forsberg <j.forsberg65@outlook.com>
2020-05-07 16:22:30 +0200

    UnParkViewModel translated till svenska

--- Garage_2.0/Models/UnParkViewModel.cs
+++ Garage_2.0/Models/UnParkViewModel.cs
@@ -8,17 +8,22 @@ namespace Garage_2._0.Models
 {
     public class UnParkViewModel
     {
+        [Display(Name = "Regnummer")]
         public string RegNum { get; set; }
-
+        [Display(Name = "Typ")]
         public EnumType VehicleType { get; set; }
-
+        [Display(Name = "Ankomst")]
         public DateTime ArrivalTime { get; set; }
 
+        [Display(Name = "Lämnar")]
         public DateTime DepartureTime { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:d\\ hh\\:mm}")]
+        [Display(Name = "Parkeringstid")]
         public TimeSpan TotalParkedTime { get; set; }
 
+        [Display(Name = "Pris")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public Decimal Price { get; set; }
 
     }
