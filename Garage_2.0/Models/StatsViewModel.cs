ab124f13668f04e11826184b001724fcc629e046
Josefin H <josefin.hallsson@gmail.com>
2020-05-11 14:26:17 +0200

    added data annotations, fixed view

--- Garage_2.0/Models/StatsViewModel.cs
+++ Garage_2.0/Models/StatsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,15 @@ namespace Garage_2._0.Models
     {
         public Dictionary<EnumType,int> AmountOfVehicleTypes { get; set; }
 
+        [Display(Name = "Totalt antal fordon")]
         public int TotalVehicles { get; set; }
-
+        [Display(Name = "Totalt antal hjul")]
         public int TotalWheels { get; set; }
-
+        [Display(Name = "Totalta intäkter")]
         public float TotalGeneratedIncome { get; set; }
-
+        [Display(Name = "Vanligaste färgen")]
         public EnumColor MostCommonColor { get; set; }
-
+        [Display(Name = "Längsta parkering")]
         public DateTime OldestParkTime { get; set; }
     }
 }
